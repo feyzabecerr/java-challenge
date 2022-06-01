8bd6c062003c42ead86d503afa0b06bc8fd06569
feyzabecerr <19120205036@ismu.edu.tr>
2022-06-01 17:45:06 +0300

    Phone.java fixed

--- src/main/java/com/kodgemisi/usermanagement/Phone.java
+++ src/main/java/com/kodgemisi/usermanagement/Phone.java
@@ -9,6 +9,36 @@ public class Phone implements CharSequence {
 
 	public Phone(String phoneNumber) {
 		this.phoneNumber = phoneNumber == null ? "" : phoneNumber;
+		if(phoneNumber == null){
+			throw new IllegalArgumentException("A phone number cannot be null");
+		}
+		else{
+			checkEmpty(phoneNumber);
+			checkSign();
+		}
+	}
+
+	public void checkEmpty(String phoneNumber){
+		if(phoneNumber.isEmpty()){
+			throw new IllegalArgumentException("A phone number cannot be empty");
+		}
+
+	}
+
+	public void checkSign(){
+		if(charAt(0) == '+'){
+			if(length() < 5){
+				throw new IllegalArgumentException("A phone number should be at least 5-char long when it starts with +");
+			}
+			if(subSequence(0, 3).equals("+90") && length()!= 13){
+				throw new IllegalArgumentException("should be exactly 13-char long when it starts with `+90`");	
+			}
+		}
+		else{
+			if(!(phoneNumber.equals("112") || phoneNumber.equals("911"))){
+				throw new IllegalArgumentException("may not start with `+` if it's `112` or `911`");
+			}
+		}
 	}
 
 	public String number() {
@@ -49,4 +79,5 @@ public class Phone implements CharSequence {
 	public int hashCode() {
 		return phoneNumber.hashCode();
 	}
+
 }
